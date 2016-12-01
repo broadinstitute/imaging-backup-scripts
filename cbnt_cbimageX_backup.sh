b6b87ade07a0caf05357176a92926b8c23f71533
Shantanu Singh <shsingh@broadinstitute.org>
2016-12-01 16:24:34 -0500

    Exclude

--- cbnt_cbimageX_backup.sh
+++ cbnt_cbimageX_backup.sh
@@ -6,14 +6,22 @@ destination_dir=/imaging/cold/cbnt_cbimageX_backup
 subdir=xiaoyunwu
 dir_list=`find ${source_dir}/$subdir -maxdepth 1 -mindepth 1 -type d`
 mkdir -p ${destination_dir}/${subdir}
+
 # 2 hour limit (as of 2016/11).
 QUEUE=short
 # No time limit (as of 2016/11)
 #QUEUE=long
 
+EXCLUDE_FILE=exclude.txt
+
 for dir in $dir_list;
 do
-    file=`basename $dir`
-    qsub -q ${QUEUE} -cwd -o ${destination_dir}/${subdir}/x${file}.log -N x${file} -j y -b y -V "tar cvf - ${dir} | gzip --fast > ${destination_dir}/${subdir}/${file}.tar.gz"
+    if `grep -Fxq $dir $EXCLUDE_FILE`
+    then
+	echo Skipping $dir
+    else
+	file=`basename $dir`
+	echo qsub -q ${QUEUE} -cwd -o ${destination_dir}/${subdir}/x${file}.log -N x${file} -j y -b y -V "tar cvf - ${dir} | gzip --fast > ${destination_dir}/${subdir}/${file}.tar.gz"
+    fi
 done
 
