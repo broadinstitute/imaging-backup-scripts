4c29e54d52cc2339b27867b112d7b6d44063fac3
Shantanu Singh <shsingh@broadinstitute.org>
2016-12-01 14:35:03 -0500

    cannot start with a digit

--- cbnt_cbimageX_backup.sh
+++ cbnt_cbimageX_backup.sh
@@ -7,6 +7,6 @@ mkdir -p ${destination_dir}/${subdir}
 for dir in $dir_list;
 do
     file=`basename $dir`
-    qsub -cwd -o ${destination_dir}/${subdir}/${file}.log  -j y -b y -V "tar cvf - ${dir} | gzip --fast > ${destination_dir}/${subdir}/${file}.tar.gz"
+    qsub -cwd -o ${destination_dir}/${subdir}/x${file}.log  -j y -b y -V "tar cvf - ${dir} | gzip --fast > ${destination_dir}/${subdir}/${file}.tar.gz"
 done
 
