148ea54fec90d0ef668a539986e8cf09bf15898d
Shantanu Singh <shsingh@broadinstitute.org>
2016-12-01 14:52:27 -0500

    select queue

--- cbnt_cbimageX_backup.sh
+++ cbnt_cbimageX_backup.sh
@@ -3,10 +3,14 @@ destination_dir=/imaging/cold/cbnt_cbimageX_backup
 subdir=xiaoyunwu
 dir_list=`find ${source_dir}/$subdir -maxdepth 1 -mindepth 1 -type d`
 mkdir -p ${destination_dir}/${subdir}
+# 2 hour limit (as of 2016/11)
+QUEUE=short
+# No time limit (as of 2016/11)
+#QUEUE=long
 
 for dir in $dir_list;
 do
     file=`basename $dir`
-    qsub -cwd -o ${destination_dir}/${subdir}/x${file}.log -N x${file} -j y -b y -V "tar cvf - ${dir} | gzip --fast > ${destination_dir}/${subdir}/${file}.tar.gz"
+    qsub -q ${QUEUE} -cwd -o ${destination_dir}/${subdir}/x${file}.log -N x${file} -j y -b y -V "tar cvf - ${dir} | gzip --fast > ${destination_dir}/${subdir}/${file}.tar.gz"
 done
 
