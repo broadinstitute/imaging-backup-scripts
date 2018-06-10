7dd8993a9113b1724bde3dbd928b291708a21584
Shantanu Singh <shsingh@broadinstitute.org>
2018-06-09 20:04:39 -0400

    split into two tar files

--- aws_backup.sh
+++ aws_backup.sh
@@ -1,12 +1,17 @@
-# This script create a single tarball for each plate of data. The tarball contains
+# This script create two tarballs for each plate of data.
+# The first tarball (with suffix _images_illum_analysis) contains
 # - images (typically ~230Gb)
 # - illumination functions (tiny)
 # - CellProfiler measurements as CSV files (typically ~30Gb)
+#
+# The second tarball (with suffix _backend) contains
 # - SQLite backend created by ingesting the CSV files (typically ~24Gb)
+# - CSV and GCT files created by processing the SQLite backend (tiny)
 #
-# The tar ball is stored at this location
+# The tar balls are stored at this location
 #
-# s3://imaging-platform-cold/imaging_analysis/<PROJECT_NAME>/plates/<PROJECT_NAME>_<BATCH_ID>_<PLATE_ID>.tar.gz
+# s3://imaging-platform-cold/imaging_analysis/<PROJECT_NAME>/plates/<PROJECT_NAME>_<BATCH_ID>_<PLATE_ID>_images_illum_analysis.tar.gz
+# s3://imaging-platform-cold/imaging_analysis/<PROJECT_NAME>/plates/<PROJECT_NAME>_<BATCH_ID>_<PLATE_ID>_backend.tar.gz
 #
 # e.g.
 # .
@@ -14,12 +19,15 @@
 #     └── imaging_analysis
 #         └── 2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad
 #             └── plates
-#                 ├── 2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad_2017_12_05_Batch2_BR00092655.tar.gz
+#                 ├── 2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad_2017_12_05_Batch2_BR00092655_images_illum_analysis.tar.gz
+#                 ├── 2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad_2017_12_05_Batch2_BR00092655_backend.tar.gz
 #                 ├── ...
-#                 └── 2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad_2017_12_05_Batch2_BR00092789.tar.gz
+#                 ├── 2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad_2017_12_05_Batch2_BR00092789_images_illum_analysis.tar.gz
+#                 └── 2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad_2017_12_05_Batch2_BR00092789_backend.tar.gz
 #
+# The corresponding md5 files are also stored alongside the tar.gz files
 #
-# When 2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad_2017_12_05_Batch2_BR00092655.tar.gz is unzipped,
+# When 2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad_2017_12_05_Batch2_BR00092655_*.tar.gz files are unzipped,
 # the directory structure will look like this
 # .
 # └── 2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad
@@ -87,6 +95,13 @@ do
     shift
 done
 
+# project_name=2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad
+# batch_id=2017_12_05_Batch2
+# plate_id_full="BR00092655__2017-12-10T12_48_16-Measurement 1"
+# plate_id=BR00092655
+# tmpdir=~/ebs_tmp
+
+
 bucket="${bucket:-imaging-platform}"
 cold_bucket="${cold_bucket:-imaging-platform-cold}"
 tmpdir="${tmpdir:-/tmp}"
@@ -95,6 +110,7 @@ s3_prefix=s3://${bucket}/projects/${project_name}
 s3_cold_prefix=s3://${cold_bucket}/imaging_analysis/${project_name}/plates
 s3_cold_prefix_key=imaging_analysis/${project_name}/plates
 plate_archive_tag=${project_name}_${batch_id}_${plate_id}
+script_dir=$(pwd)
 
 # report sizes
 # s3cmd du "${s3_prefix}/${batch_id}/images/${plate_id_full}"
@@ -150,41 +166,69 @@ aws s3 sync "${s3_prefix}/workspace/backend/${batch_id}/${plate_id}" "workspace/
 
 cd ../../
 
-# create tarball
+function process_tar_file {
 
-tar -czf ${plate_archive_tag}.tar.gz ${plate_archive_tag} || { echo 'my_command failed' ; exit 1; }
+    tar_file=$1
 
-file_listing_tar=${plate_archive_tag}_file_listing_tar.txt
+    file_listing_tar=${tar_file}_file_listing_tar.txt
 
-tar -tzf ${plate_archive_tag}.tar.gz | awk -F/ '{ if($NF != "") print }' > ${file_listing_tar}
+    tar -tzf ${tar_file}.tar.gz | awk -F/ '{ if($NF != "") print }' > ${file_listing_tar}
 
-# calculate md5
+    # calculate md5
 
-md5sum ${plate_archive_tag}.tar.gz > ${plate_archive_tag}.md5
+    md5sum ${tar_file}.tar.gz > ${tar_file}.md5
 
-# copy to S3
-aws s3 cp ${plate_archive_tag}.tar.gz ${s3_cold_prefix}/${plate_archive_tag}.tar.gz
+    # copy to S3
 
-# check whether local ETag and remote Etag match
-# https://github.com/antespi/s3md5
-etag_local=$(./s3md5 8 ${plate_archive_tag}.tar.gz)
+    aws s3 cp ${tar_file}.tar.gz ${s3_cold_prefix}/${tar_file}.tar.gz
 
-etag_remote=$(aws s3api head-object --bucket ${cold_bucket} --key ${s3_cold_prefix_key}/${plate_archive_tag}.tar.gz |jq '.ETag' -|tr -d '"'|tr -d '\\')
+    # check whether local ETag and remote Etag match
+    # https://github.com/antespi/s3md5
 
-if [ "$etag_local" != "$etag_remote" ]; then
-  echo "Remote and local ETags don't match"
-  echo "Remote =" $etag_remote
-  echo "Local  =" $etag_local
-  exit 1
-fi
+    etag_local=$(${script_dir}/s3md5 8 ${tar_file}.tar.gz)
 
-# copy md5 to remote
+    etag_remote=$(aws s3api head-object --bucket ${cold_bucket} --key ${s3_cold_prefix_key}/${tar_file}.tar.gz |jq '.ETag' -|tr -d '"'|tr -d '\\')
 
-aws s3 cp ${plate_archive_tag}.md5 ${s3_cold_prefix}/${plate_archive_tag}.md5
+    if [ "$etag_local" != "$etag_remote" ]; then
+      echo "Remote and local ETags don't match"
+      echo "Remote =" $etag_remote
+      echo "Local  =" $etag_local
+      exit 1
+    fi
 
-# remove local cache of tarball and md5
+    # copy md5 to remote
 
-rm -rf ${plate_archive_tag} ${plate_archive_tag}.tar.gz ${plate_archive_tag}.md5
+    aws s3 cp ${tar_file}.md5 ${s3_cold_prefix}/${tar_file}.md5
+
+    # remove local cache of tarball and md5
+
+    rm -rf ${tar_file}.tar.gz ${tar_file}.md5
+
+}
+
+# create tarball for images, illum, analysis folders
+
+tar_file=${plate_archive_tag}_images_illum_analysis
+
+tar -czf ${tar_file}.tar.gz \
+  "${plate_archive_tag}/${project_name}/${batch_id}/images/${plate_id_full}" \
+  "${plate_archive_tag}/${project_name}/${batch_id}/illum/${plate_id}" \
+  "${plate_archive_tag}/${project_name}/workspace/analysis/${batch_id}/${plate_id}"
+
+process_tar_file ${tar_file}
+
+# create tarball for backend folders
+
+tar_file=${plate_archive_tag}_backend
+
+tar -czf ${tar_file}.tar.gz \
+  "${plate_archive_tag}/${project_name}/workspace/backend/${batch_id}/${plate_id}"
+
+process_tar_file ${tar_file}
+
+# remove downloaded files
+
+rm -rf ${plate_archive_tag}
 
 # remove files from S3
 
