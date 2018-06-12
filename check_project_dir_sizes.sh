3c4225849387f35b1ffd66bc3d912f2c5f5b4afb
Shantanu Singh <shsingh@broadinstitute.org>
2018-06-12 05:34:06 -0400

    Update check_project_dir_sizes.sh

--- check_project_dir_sizes.sh
+++ check_project_dir_sizes.sh
@@ -1,5 +1,8 @@
 # Check sizes of project directories in s3://imaging-platform/projects/
 parallel \
   s3cmd du s3://imaging-platform/projects/{1} ::: \
-  `aws s3 ls s3://imaging-platform/projects/ | grep PRE | tr -s " " |cut -d" " -f3 | cut -d"/" -f1` > \
+  `aws s3 ls s3://imaging-platform/projects/ | grep PRE | tr -s " " |cut -d" " -f3 | cut -d"/" -f1` | \
+  sort -nr | \
+  awk '{ print $2 "," $1 }' | \
+  cut -d"/" -f5 > \
   imaging-platform_project_sizes.txt
